c296c57e954d00900245d1cf603cb5c80cd36f0b
Gadgeter-EA <emiacevedoposos@gmail.com>
2023-04-29 13:08:26 -0600

    Initial Commit

---
+++ park-in/park_inApp.swift
@@ -0,0 +1,17 @@
+//
+//  park_inApp.swift
+//  park-in
+//
+//  Created by iOS Lab on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct park_inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
